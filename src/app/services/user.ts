2c2cf198b79b4718bf40847610ca550df5c459b9
Anthony <info@anthonyscheeres.nl>
2019-12-17 13:53:06 +0100

    .

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -5,9 +5,13 @@ import { AccountModel } from '../models/AccountModel';
 import { fetchJsonPost, fetchJsonGet } from './http';
 import { UserModel } from '../models/UserModel';
 
+
+var host = ServerModel.host;
+var port = ServerModel.port;
+var token = AccountModel.token
+
 export function loadUsers() {
-  var host = ServerModel.host;
-  var port = ServerModel.port;
+
   var token = AccountModel.token;
   var url = "http://" + host + ":" + port + "/user/show";
   return url
@@ -16,8 +20,6 @@ export function loadUsers() {
 
 
 export function login(username, password) {
-  var host = ServerModel.host;
-  var port = ServerModel.port;
   var url = "http://" + host + ":" + port + "/user/login";
 
   var data = JSON.stringify({
@@ -41,8 +43,6 @@ export function login(username, password) {
 
 export function register(username, password, email) {
 
-  var host = ServerModel.host;
-  var port = ServerModel.port;
   var urlToServer = "http://" + host + ":" + port + "/user/createUser";
 
   var data = JSON.stringify({
@@ -59,12 +59,9 @@ export function register(username, password, email) {
 }
 
 export function getUsers() {
-  var host = ServerModel.host;
-  var port = ServerModel.port;
-  var token = AccountModel.token
+
   var url = "http://" + host + ":" + port + "/user/" + token + "/showAllUsers";
 
-  //ToDo: make it so that the token is loaded from the user itself
 
   return url;
 }
@@ -73,16 +70,68 @@ export function getUsers() {
 
 export function deleteUser(user: UserModel) {
 
+  var urlToServer = "http://" + host + ":" + port + "/user/" + token + "/remove";
+
+ var username = UserModel.username
+  var data = JSON.stringify({
+    "username": username,
+    "password": null,
+    "id": null,
+    "permission": null,
+    "email": null
+  });
+
+
+  return fetchJsonPost(urlToServer, data.toString())
 }
 
 export function giveRead(user: UserModel) {
 
+  var urlToServer = "http://" + host + ":" + port + "/user/" + token + "/read";
+
+  var username = UserModel.username
+  var data = JSON.stringify({
+    "username": username,
+    "password": null,
+    "id": null,
+    "permission": null,
+    "email": null
+  });
+
+
+  return fetchJsonPost(urlToServer, data.toString())
 }
 
 export function giveWrite(user: UserModel) {
 
+  var urlToServer = "http://" + host + ":" + port + "/user/" + token + "/write";
+
+  var username = UserModel.username
+  var data = JSON.stringify({
+    "username": username,
+    "password": null,
+    "id": null,
+    "permission": null,
+    "email": null
+  });
+
+
+  return fetchJsonPost(urlToServer, data.toString())
 }
 
 export function giveDelete(user: UserModel) {
 
+  var urlToServer = "http://" + host + ":" + port + "/user/"+token +"/delete";
+
+  var username = UserModel.username
+  var data = JSON.stringify({
+    "username": username,
+    "password": null,
+    "id": null,
+    "permission": null,
+    "email": null
+  });
+
+
+  return fetchJsonPost(urlToServer, data.toString())
 }
