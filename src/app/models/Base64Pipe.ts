56c9f4c2c5044ab0d30e51b8159531ca7b809a53
Anthony <info@anthonyscheeres.nl>
2019-12-19 15:57:43 +0100

    .

---
+++ src/app/models/Base64Pipe.ts
@@ -0,0 +1,14 @@
+import { PipeTransform, Pipe } from '@angular/core';
+
+@Pipe({
+  name: 'decode64'
+})
+export class Decode64Pipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    let a = '';
+    if (value) {
+      a = atob(value);
+    }
+    return a;
+  }
+}
