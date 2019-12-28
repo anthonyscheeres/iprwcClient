af40aa1fa224266eee7d0decb56a003f25ad55f8
Anthony <info@anthonyscheeres.nl>
2019-12-28 11:50:29 +0100

    .

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { register } from '../services/user';
 import { responseR } from '../models/ResponseRequest';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -9,7 +10,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  myRecaptcha: boolean = false;
 
+  onScriptLoad() {
+    console.log('Google reCAPTCHA loaded and is ready for use!')
+  }
+
+  onScriptError() {
+    this.myRecaptcha = true
+  }
   constructor(private _router: Router) { }
 s
   ngOnInit() {
@@ -18,19 +27,23 @@ s
 
 
   async registerUser(event) {
+
     event.preventDefault()
-    const target = event.target
+    if (this.myRecaptcha) {
 
-    const username = target.querySelector('#username').value
-    const password = target.querySelector('#password').value
-    const email = target.querySelector('#email').value
-    await register(username, password, email).then(response => {
+      const target = event.target
 
-      if (response != responseR.fail) {
-        this._router.navigate(['/login']);
+      const username = target.querySelector('#username').value
+      const password = target.querySelector('#password').value
+      const email = target.querySelector('#email').value
+      await register(username, password, email).then(response => {
 
-      }
-    });
+        if (response != responseR.fail) {
+          this._router.navigate(['/login']);
+
+        }
+      })
+    };
 
 
   }
